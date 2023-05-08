5e810cb921e10cdca93acf9515b85f0d078bfaeb
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-05-08 18:45:29 +0200

    Added dependencies to aws-lambda in go project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Input type is input
+type Input struct {
+	Name string `json:name`
+}
+
+// Output is output
+type Output struct {
+	Message string
+}
+
+func handler(input Input) (Output, error) {
+	fmt.Printf("Received input: %v\n", input)
+	return Output{
+		Message: "Hello" + input.Name + "!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
